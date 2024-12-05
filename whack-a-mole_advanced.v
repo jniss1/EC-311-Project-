29f8fa5cd9ab798de9378e3540cc5897d547ca42
jniss1 <113002485+jniss1@users.noreply.github.com>
2024-12-05 17:45:56 -0500

    Sorta works...? (Update below)
    Hi everyone,
    here is the update:
    
    Though each mole/button pair works individually when the timers are hard-coded, they are faulty when using the random timers (Also, its now using the built in randomizer instead of the external module since it was having trouble working with it).
    
    What works:
    
    - Multi-mole testbench has the proper values when manually shortining the mole_timers (commented out code), both for the individual and ramdomized versions.
    
    - Dynamic hammer timer shortening should work, but Vivado decided to be sad as I was going to test it (detailed below)
    
    Additional comment: before the code was running smoothly when lives and score both were 4 bit values. However, to represent the score of 100 and due to the limit of LEDs on the board, I shifted it so lives is now a 2 bit value and score is now 7bit. This required some minor changes in the constraint file, whack_a_mole_advanced, and Debounced_WAM_advanced modules. Unfortunatly, Vivado dicided to act up as I was doing this change so I was unable to verify if this changed worked (detailed below), so if synthesis failed try going back to the 4x4
    
    
    
    
    Current issues:
    
    - Vivado is being dumb and is saying the final case has bad syntax when it does not...I tried resetting and reloading but the issue seems to persist on my computer and It cannot synthesize without it being fixed. It systhesized earlier with the same line just fine, so not sure what the issue is here.
    
    - Running this code with the randomized timers causes the code to stall before any mole appears on the board. To me, this would imply an error in the logic around the if statment around line 180, however the code works fine when doing one timer at a time so again, not sure whats going bad here.
    
    - Posibly related to Vivado being dumb (not sure); game logic fails to transition from END_SCREEN state (the one Vivado is glitching on) to the IDLE state upon a button press. This issue also exists in the normal whack-a-mole code aswell.

--- whack-a-mole_advanced.v
+++ whack-a-mole_advanced.v
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module whack_a_mole(
+module whack_a_mole_advanced(
     input clk,
     input reset,
     input button,              // Debounced button input (fed from exernal module)
@@ -35,8 +35,8 @@ module whack_a_mole(
     output reg mole2,
     output reg mole3,
     output reg mole4,
-    output reg [3:0] score,    // Player score
-    output reg [3:0] lives,    // Player lives (3 max) -- binary
+    output reg [6:0] score,    // Player score
+    output reg [1:0] lives,    // Player lives (3 max) -- binary
     output reg [2:0] state     // Game state (IDLE, GAMEPLAY, END_SCREEN)
     //output reg [7:0] highscore // set to 0 on reset (thus need to fix ENDSCREEN bug); initilized to 0, updates when it is broken. Displays your score against it in END_SCREEN
 );
@@ -52,34 +52,20 @@ reg [28:0] mole_timer2;
 reg [28:0] mole_timer3;
 reg [28:0] mole_timer4;
 
-reg [28:0] hammer_timer;      // Time for hammer availability
+reg [28:0] randnum;
+reg [28:0] randnum2;
+reg [28:0] randnum3;
+reg [28:0] randnum4;
+
+
+reg [28:0] hammer_timer;     // Time for hammer availability
 reg button_prev;             // Previous button state to detect rising edge
 reg button_prev2;
 reg button_prev3;
 reg button_prev4;
-reg [7:0] blink_counter;     // Counter for blinking in the end state
-reg [7:0] random_num;        // Random number for mole_timer
-reg [28:0] reduced_speed; // NEEDS MODIFICATION, shoudl correspond to integer that is 0.083 seconds
+reg [28:0] blink_counter;   // Counter for blinking in the end state
+reg [28:0] reduced_speed;   // NEEDS MODIFICATION, shoudl correspond to integer that is 0.083 seconds
 
-
-// Note: I don't think the random number generator detailed below is working rn; As a task, turn it into extern module as input
-
-/*
-// Reset Logic for timers, button, and RNG
-always @(posedge clk or posedge reset) begin
-    if (reset) begin
-        mole_timer <= 0;        // Resetting the timer
-        hammer_timer <= 0;      // Resetting the hammer timer
-        button_prev <= 0;       // Initialize button_prev
-        random_num <= 8'd0;     // Random number
-        state <= 3'b000;
-    end else begin
-        // CLK updates for random num and button prev; timer logics will be handles in game loop for clarity.
-        button_prev <= button;          // Update button_prev on every clock cycle; helps detect rising edge (button = 1, button prev = 0, every other combo is not it)
-        random_num <= random_num + 1;   // change random number
-    end
-end
-*/
 
 //Button Tracking
 always @(posedge clk or posedge reset) begin
@@ -88,23 +74,54 @@ always @(posedge clk or posedge reset) begin
         button_prev2 <= 0;
         button_prev3 <= 0;
         button_prev4 <= 0;
+        
+//        randnum <= $urandom_range(100000000, 300000000);
+//        randnum2 <= $urandom_range(100000000, 300000000);
+//        randnum3 <= $urandom_range(100000000, 300000000);
+//        randnum4 <= $urandom_range(100000000, 300000000);
+        
+//        randnum <= $urandom_range(10, 30);
+//        randnum2 <= $urandom_range(10, 30);
+//        randnum3 <= $urandom_range(10, 30);
+//        randnum4 <= $urandom_range(10, 30);
+
+        randnum <= 100_000_001;
+        randnum2 <= 100_000_002;
+        randnum3 <= 100_000_003;
+        randnum4 <= 100_000_000;
+        
     end else begin
         button_prev <= button;  // Update button_prev on every clock cycle
         button_prev2 <= button2;
         button_prev3 <= button3;
         button_prev4 <= button4;
+        
+//        randnum <= $urandom_range(100000000, 300000000);
+//        randnum2 <= $urandom_range(100000000, 300000000);
+//        randnum3 <= $urandom_range(100000000, 300000000);
+//        randnum4 <= $urandom_range(100000000, 300000000);
+        
+//        randnum <= $urandom_range(10, 30);
+//        randnum2 <= $urandom_range(10, 30);
+//        randnum3 <= $urandom_range(10, 30);
+//        randnum4 <= $urandom_range(10, 30);
+
+        randnum <= 100_000_001;
+        randnum2 <= 100_000_002;
+        randnum3 <= 100_000_003;
+        randnum4 <= 100_000_000;
     end
 end
 
 
 
 // Game logic state machine
-always @(posedge clk or posedge reset) begin
+always @(negedge clk or posedge reset) begin
     if (reset) begin
     // Reset all variable
         state <= IDLE;
-        score <= 4'b0000;    // begin with 0 score
-        lives <= 4'b0000;    // Starting with 3 lives
+        score <= 7'b0000000;    // begin with 0 score
+        lives <= 2'b11;    // Starting with 3 lives
         
         mole <= 0;
         mole2 <= 0;
@@ -127,7 +144,7 @@ always @(posedge clk or posedge reset) begin
     end else begin
         case(state)
             IDLE: begin
-                reduced_speed <= 24_900_000;
+                reduced_speed <= 24_900_000; // Mote: this value corresponds to about 0.084 seconds, so after getting to level 90 reducing by this amount every time, reation timer is ~0.25 sec, or the average human reaction time
                 mole <= 0;
                 mole2 <= 0;
                 mole3 <= 0;
@@ -140,12 +157,12 @@ always @(posedge clk or posedge reset) begin
                 // Detect rising edge of the button, for any button, on press goto GAMPLAY on next clk cycle
                 if ((button && !button_prev) || (button2 && !button_prev2) || (button3 && !button_prev3) || (button4 && !button_prev4)) begin   
                     // Initilizing gameplay variables
-                    score <= 4'b0000;
-                    lives <= 4'b0011;
-                    mole_timer <= 100_000_001;
-                    mole_timer2 <= 100_000_000;        // Note that RNG should have contraint of outputting 4 DIFFERENT rand. nums to avoid issue of 2 timers running out at the same time
-                    mole_timer3 <= 100_000_002;
-                    mole_timer4 <= 100_000_003;
+                    score <= 7'b0000000;
+                    lives <= 2'b11;
+                    mole_timer <= randnum;
+                    mole_timer2 <= randnum2;        
+                    mole_timer3 <= randnum3;
+                    mole_timer4 <= randnum4;
                     hammer_timer <=  300_000_000;                 // 1 second for hammer timer
                     state <= GAMEPLAY;
                 end
@@ -162,9 +179,9 @@ always @(posedge clk or posedge reset) begin
                 //else if (mole_timer > 0 || mole_timer2 > 0 || mole_timer3 > 0 || mole_timer4 > 0) begin
                 else if (mole_timer > 0) begin
                     mole_timer = mole_timer - 1;
-                    mole_timer2 = mole_timer - 1;
-                    mole_timer3 = mole_timer - 1;
-                    mole_timer4 = mole_timer - 1;
+                    mole_timer2 = mole_timer2 - 1;
+                    mole_timer3 = mole_timer3 - 1;
+                    mole_timer4 = mole_timer4 - 1;
                     
                     // if mole timer has hit zero, we turn mole on, turn off other timers, and trigger the start of hammer logic
                     // MOLE 1
@@ -224,14 +241,15 @@ always @(posedge clk or posedge reset) begin
                         mole3 <= 0;
                         mole4 <= 0;
                         // NOTE: currently hardcoded for debugging, but just need to change it to equal equivilanet randnum# input
-                        mole_timer <= 100_000_000;
-                        mole_timer2 <= 100_000_001;
-                        mole_timer3 <= 100_000_002;
-                        mole_timer4 <= 100_000_003;
+                        mole_timer <= randnum;
+                        mole_timer2 <= randnum2;        
+                        mole_timer3 <= randnum3;
+                        mole_timer4 <= randnum4;
                         
                         hammer_timer <= 300_000_000;
                         // DYNAMIC TIMER INCREASE
-                        if (score > 5) begin
+                        if (score > 10) begin
+                        if (score > 10) begin
                                 hammer_timer <= hammer_timer - reduced_speed;
                                 if (score > 20) begin
                                         hammer_timer <= hammer_timer - reduced_speed;
@@ -273,10 +291,10 @@ always @(posedge clk or posedge reset) begin
                         
                             else begin
                                 //NOTE: currently hardcoded for debugging, but just need to change it to equal equivilanet randnum# input
-                                mole_timer <=  100_000_000;  
-                                mole_timer2 <= 100_000_001;  
-                                mole_timer3 <= 100_000_002;  
-                                mole_timer4 <= 100_000_003;  
+                                mole_timer <= randnum;
+                                mole_timer2 <= randnum2;        
+                                mole_timer3 <= randnum3;
+                                mole_timer4 <= randnum4; 
                                 hammer_timer <=  300_000_000;                 
                             end
                    end
@@ -299,37 +317,39 @@ always @(posedge clk or posedge reset) begin
                     
                     // If that did not trigger, then there are still lives remaining. Reset the timers to begin gameplay loop all over again
                     else begin
-                        mole_timer <=  200_000_000;  // Again, signal to MODIFY THIS in accordance to new RNG module
-                        mole_timer2 <=  200_000_001;
-                        mole_timer3 <=  200_000_002;
-                        mole_timer4 <=  200_000_003;
+                        mole_timer <= randnum;
+                        mole_timer2 <= randnum2;        
+                        mole_timer3 <= randnum3;
+                        mole_timer4 <= randnum4;
                         hammer_timer <=  300_000_000;                 
                     end
                 end
             end
             
-            END_SCREEN: begin
+            3'b010: begin
                 // Mole LED will blink every second until button is pressed
-                /*blink_counter <= blink_counter + 1;
+                blink_counter <= blink_counter + 1;
                 if (blink_counter == 300_000_000) begin  // Toggle mole every second (assuming 100 MHz clock)
                     mole <= ~mole;                       // Toggle mole visibility
                     blink_counter <= 0;                  // reset counter for next time
-                end*/
+                end
+                
                 //if (score > highscore) begin
                     //highscore <= score
                 //end
-                score <= 4'b0000;
+                
+                score <= 7'b0000000;
                 // Waits in this state until a rising edge button press is detected, then sends to IDLE state.
                 if (button && !button_prev) begin  
                     //state <= 3'b000;  // Reset to IDLE state next clk cycle
                     // Some of the assignemnts are redudent since they happen again in the idle state, just saying. However, if you later want to trim it down, trim the ones in the IDLE state instead
-                    score <= 4'b000000;
-                    lives <= 4'b0011; // Starting with 3 lives
+                    score <= 7'b0000000;
+                    lives <= 2'b11; // Starting with 3 lives
                     mole <= 0;
                     mole_timer <= 0;
                     hammer_timer <= 0;
                     blink_counter <= 0;
-                    state <= IDLE;  // Reset to IDLE state next clk cycle
+                    state <= 3'b000;  // Reset to IDLE state next clk cycle
                 end
             end
         endcase
