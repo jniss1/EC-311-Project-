ee20062c7c417cea8cffd944a668c03f1462c080
jniss1 <113002485+jniss1@users.noreply.github.com>
2024-12-05 17:29:04 -0500

    Update Debounced_WAM_advanced.v

--- Debounced_WAM_advanced.v
+++ Debounced_WAM_advanced.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/25/2024 04:33:38 PM
-// Design Name: 
-// Module Name: Debounced_WAM
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module Debounced_WAM_advanced(
     input clk,
     input reset,
@@ -33,8 +11,8 @@ module Debounced_WAM_advanced(
     output wire mole3, 
     output wire mole4,           // Mole visibility (1 for mole shown, 0 for hidden)
     
-    output wire [3:0] score,    // Player score
-    output wire [3:0] lives,    // Player lives
+    output wire [6:0] score,    // Player score
+    output wire [1:0] lives,    // Player lives
     output wire [2:0] state     // Game state (IDLE, GAMEPLAY, END_SCREEN)
     );
     
@@ -48,5 +26,5 @@ module Debounced_WAM_advanced(
     debouncer DEB3(clk, button3, clean3);
     debouncer DEB4(clk, button4, clean4);
     
-    whack_a_mole_advanced(clk, reset, clean, clean2, clean3, clean4, mole, mole2, mole3, mole4, score, lives, state);
+    whack_a_mole_advanced DEBWAM(clk, reset, clean, clean2, clean3, clean4, mole, mole2, mole3, mole4, score, lives, state);
 endmodule
