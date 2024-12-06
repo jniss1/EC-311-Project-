fc10cfeb67689ef67ccfef64f0858be7dd490a2b
jniss1 <113002485+jniss1@users.noreply.github.com>
2024-12-06 14:38:24 -0500

    can synthesize, but incomplete...
    This code can generate a bitstream with the given modules. However, in order to bypass the bug in end state, the code there was commented out and it goes back to the idle state. Also, the dynamic hammer timer doe not seem to be working.

--- whack-a-mole_advanced.v
+++ whack-a-mole_advanced.v
@@ -121,7 +121,7 @@ always @(negedge clk or posedge reset) begin
     // Reset all variable
         state <= IDLE;
         score <= 7'b0000000;    // begin with 0 score
-        lives <= 2'b11;    // Starting with 3 lives
+        lives <= 2'b00;    // Starting with 3 lives
         
         mole <= 0;
         mole2 <= 0;
@@ -171,7 +171,8 @@ always @(negedge clk or posedge reset) begin
             GAMEPLAY: begin 
                 // First, we will check if the game is over:
                 if (lives == 0) begin
-                        state <= 3'b010;  // GLITCH: breaks when it says end screen Go to end screen if no lives left. IDK why...
+                        //state <= 3'b010;  // GLITCH: breaks when it says end screen Go to end screen if no lives left. IDK why...
+                        state <= IDLE;
                 end 
                 
                 // Next, we decrement the new mole timer, and check if it has hit 0:
@@ -248,7 +249,7 @@ always @(negedge clk or posedge reset) begin
                         
                         hammer_timer <= 300_000_000;
                         // DYNAMIC TIMER INCREASE
-                        if (score > 10) begin
+                    
                         if (score > 10) begin
                                 hammer_timer <= hammer_timer - reduced_speed;
                                 if (score > 20) begin
@@ -286,7 +287,8 @@ always @(negedge clk or posedge reset) begin
                             mole3 <= 0;
                             mole4 <= 0;
                             if (lives == 0) begin
-                                state <= END_SCREEN;  // Go to end screen if no lives left
+                                //state <= END_SCREEN;  // Go to end screen if no lives left
+                                state <= IDLE;
                             end 
                         
                             else begin
@@ -312,7 +314,8 @@ always @(negedge clk or posedge reset) begin
                     
                     // if this triggers, goto END SCREEN the next clk cycle (the other variable resets will happen later)
                     if (lives == 0) begin
-                        state <= END_SCREEN;  // Go to end screen if no lives left
+                        //state <= END_SCREEN;  // Go to end screen if no lives left
+                        state <= IDLE;
                     end 
                     
                     // If that did not trigger, then there are still lives remaining. Reset the timers to begin gameplay loop all over again
@@ -326,33 +329,35 @@ always @(negedge clk or posedge reset) begin
                 end
             end
             
-            3'b010: begin
-                // Mole LED will blink every second until button is pressed
-                blink_counter <= blink_counter + 1;
-                if (blink_counter == 300_000_000) begin  // Toggle mole every second (assuming 100 MHz clock)
-                    mole <= ~mole;                       // Toggle mole visibility
-                    blink_counter <= 0;                  // reset counter for next time
-                end
+            
+            
+//            END_SCREEN: begin
+//                // Mole LED will blink every second until button is pressed
+//                blink_counter <= blink_counter + 1;
+//                if (blink_counter == 300_000_000) begin  // Toggle mole every second (assuming 100 MHz clock)
+//                    mole <= ~mole;                       // Toggle mole visibility
+//                    blink_counter <= 0;                  // reset counter for next time
+//                end
                 
-                //if (score > highscore) begin
-                    //highscore <= score
-                //end
+//                //if (score > highscore) begin
+//                    //highscore <= score
+//                //end
                 
-                score <= 7'b0000000;
-                // Waits in this state until a rising edge button press is detected, then sends to IDLE state.
-                if (button && !button_prev) begin  
-                    //state <= 3'b000;  // Reset to IDLE state next clk cycle
-                    // Some of the assignemnts are redudent since they happen again in the idle state, just saying. However, if you later want to trim it down, trim the ones in the IDLE state instead
-                    score <= 7'b0000000;
-                    lives <= 2'b11; // Starting with 3 lives
-                    mole <= 0;
-                    mole_timer <= 0;
-                    hammer_timer <= 0;
-                    blink_counter <= 0;
-                    state <= 3'b000;  // Reset to IDLE state next clk cycle
-                end
-            end
-        endcase
+//                score <= 7'b0000000;
+//                // Waits in this state until a rising edge button press is detected, then sends to IDLE state.
+//                if (button && !button_prev) begin  
+//                    //state <= 3'b000;  // Reset to IDLE state next clk cycle
+//                    // Some of the assignemnts are redudent since they happen again in the idle state, just saying. However, if you later want to trim it down, trim the ones in the IDLE state instead
+//                    score <= 7'b0000000;
+//                    lives <= 2'b11; // Starting with 3 lives
+//                    mole <= 0;
+//                    mole_timer <= 0;
+//                    hammer_timer <= 0;
+//                    blink_counter <= 0;
+//                    state <= 3'b000;  // Reset to IDLE state next clk cycle
+//                end
+            //end
+       endcase
     end
 end
 
