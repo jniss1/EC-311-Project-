916d0b66293cef798b2cb34790e837d6aeb213d7
ArielleMaravilla <amaravil@bu.edu>
2024-12-10 16:49:35 -0500

    working on it

--- Code/new vga/vga_top.v
+++ Code/new vga/vga_top.v
@@ -20,21 +20,46 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module vga_top(clk, reset, in_r, in_g, in_b, vga_r, vga_g, vga_b, h_sync, v_sync);
+module vga_top(
+    input clk,
+    input reset,
+    input mole, mole2, mole3, mole4,
+    output reg [3:0] vga_r, vga_g, vga_b,
+    output h_sync, v_sync
+);
 
-    input clk, reset;
-    input [3:0] in_r, in_g, in_b;
     
-    output reg [3:0] vga_r, vga_g, vga_b;
-    output h_sync, v_sync;
     wire newClk, ledOn;
     wire [9:0] x;
     wire [9:0] y;
 
     
+    
+    
+    
+   /////Instantiations///// 
+    
+    
+    
+    //Clock Divider
     clk_divider clkDiv (clk, reset, newClk);
     
+    //VGA Controller
     vga_controller vga_con (newClk, h_sync, v_sync, ledOn, x, y);
+    
+    
+    
+    //////VGA Logic//////
+    
+//    For debugging: Hardcoded mole signals for testing
+//    reg mole, mole2, mole3, mole4;
+    
+//    initial begin
+//        mole = 1;
+//        mole2 = 0;
+//        mole3 = 1;
+//        mole4 = 0;
+//    end 
     
     // Block detection
     wire [2:0] block;
@@ -54,22 +79,22 @@ module vga_top(clk, reset, in_r, in_g, in_b, vga_r, vga_g, vga_b, h_sync, v_sync
 //                vga_b <= 4'hF;  // No blue
 //            end
 //            else 
-            if (block == 3'b001) begin
+            if (block == 3'b001 && mole) begin
                 vga_r <= 4'h0;  // No red
                 vga_g <= 4'hF;  // Full green
                 vga_b <= 4'h0;  // No blue
             end
-            else if (block == 3'b010) begin
+            else if (block == 3'b010 && mole2) begin
                 vga_r <= 4'h0;  // No red
                 vga_g <= 4'hF;  // No green
                 vga_b <= 4'h0;  // Full blue
             end
-            else if (block == 3'b011) begin
+            else if (block == 3'b011 && mole3) begin
                 vga_r <= 4'h0;  // Full red
                 vga_g <= 4'hF;  // Full green
                 vga_b <= 4'h0;  // White
             end
-            else if (block == 3'b100) begin
+            else if (block == 3'b100 && mole4) begin
                 vga_r <= 4'h0; 
                 vga_g <= 4'hF;
                 vga_b <= 4'h0; 
